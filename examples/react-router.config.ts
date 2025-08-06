eedb999a7e5fbd5dc417dece0ca1e6575da791c7
coji <coji@techtalk.jp>
2025-08-07 00:11:51 +0900

    fix: remove unused appDirectory property from react-router configuration

--- examples/react-router.config.ts
+++ examples/react-router.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from '@react-router/dev/config'
 
 export default {
-  appDirectory: "app",
   ssr: true,
-} satisfies Config;
+} satisfies Config
